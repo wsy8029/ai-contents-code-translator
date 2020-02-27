408a2b9745615b9b75cb2ab9575a7336afa90038
박다정 <dajungpark@bagdajeong-ui-MacBookPro.local>
2020-02-27 17:46:16 +0900

    Update dictionary

--- src/modi_nlp.py
+++ src/modi_nlp.py
@@ -4,27 +4,115 @@ import time
 
 tagger = Komoran()
 
-module_dic = {
+input_module_dic = {
             #sensor
             "버튼":"button", 
-            #actuator
-            "모터":"motor","바퀴":"motor",
-            "led":"led","불":"led",
-            #sensing
-            "누르":".get_clicked()",
-            #acting
-            "켜":".set_red(255)",
 
-            #initialize module
-            "led":".set_off()"
+            "다이얼":"dial",
+
+            "환경":"env", "environment":"env",
+
+            "gyroscope":"gyro", "자이로":"gyro", "자이로스코프":"gyro",
+
+            "infrared":"ir", "적외선":"ir",
+
+            "마이크":"mic",
+
+            "울트라소닉":"ultrasonic", "초음파":"ultrasonic"
+            }
+
+output_module_dic = {
+            #actuator
+            "디스플레이":"display", "화면":"display",
+
+            "불":"led",
             
+            "모터":"motor",
+
+            "스피커":"speaker"
+            }
+
+input_method_dic = {
+            #button
+            "클릭":".get_clicked()",
+            "두번 클릭":".get_double_clicked()", "더블 클릭":".get_double_clicked()"
+            "누르":".get_pressed()",
+            "켜지":".get_toggled()",
+
+            #motor
+            "각도":".get_degree()",
+            "회전속도":".get_turnspeed()",
+
+            #env
+            "온도":".get_temperature()",
+            "습도":".get_humidity()",
+            "밝기":".get_brightness()",
+            "빨강":".get_red()",
+            "초록":".get_green()",
+            "파랑":".get_blue()",
+
+            #gyro
+            "롤":".get_roll()",
+            "피치":".get_picth()",
+            "요":".get_yaw()",
+            "x축 각속도":".get_angular_vel_x()",
+            "y축 각속도":".get_angular_vel_y()",
+            "z축 각속도":".get_angular_vel_z()",
+            "x축 각속도":".get_acceleration_x()",
+            "y축 각속도":".get_acceleration_y()",
+            "z축 각속도":".get_acceleration_z()",
+            "진동":".get_vibration()",
+
+            #ir, ultrasonic
+            "거리":".get_distance()",
+
+            #mic
+            "음량":".get_volume()",
+            "진동수":".get_frequency()"
+            }
+
+output_method_dic = {
+            #display
+            # "띄워":".set_text(,"
+            "비워":".set_clear()",
+
+            #led
+            # "rgb":".set_rgb(",
+            "켜":".set_on()",
+            "꺼":".set_off()",
+            "빨강":".set_red()",
+            "초록":".set_green()",
+            "파랑":".set_blue()",
+
+            #motor
+            # "첫번째 각도":".set_first_degree(",
+            # "두번째 각도":".set_second_degree(",
+            # "첫번째 속도":".set_first_speed(",
+            # "두번째 속도":".set_second_speed(",
+            # "첫번째 토크":".set_first_torque(",
+            # "두번째 토크":".set_second_torque(",
+            # "토크":".set_torque(",
+            # "속도":".set_speed(",
+            # "각도":".set_degree(",
+
+            #speaker
+            # "음정":".set_tune(",
+            # "진동수":".set_frequency(",
+            # "음량":".set_volume(",
             }
 
 cond_dic = {
             #condition - 단일 조건
             "면":"if", "때":"if",
             #condition - 복합 조건(반대의 경우 함축적으로 포함된 경우)          
-            "만":"else"
+            "만":"else",
+            "동안":"while",
+            "보다 클":">",
+            "보다 크거나 같을":">=",
+            "일":"==",
+            "아닐":"!=",
+            "보다 작을":"<",
+            "보다 작거나 같을":"<="
             }
 
 def get_morph(sen, mor):
@@ -54,8 +142,9 @@ def main():
     bundle = modi.MODI()
 
     code = []   # 코드가 담길 배열
-    sentence = '버튼 누르면 불 켜줘'
-    #sentence = '버튼 누를때만 불켜줘'
+    sentence = input("Enter: ")
+    # sentence = '버튼 누르면 불 켜줘'
+    # sentence = '버튼 누를때만 불켜줘'
     
 
 
