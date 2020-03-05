cff3dfa9726bd24077d8ae9356f659789ec0dd14
박다정 <dajungpark@bagdajeong-ui-MacBookPro.local>
2020-03-05 18:00:15 +0900

    Add comments

--- src/code_translator.py
+++ src/code_translator.py
@@ -5,6 +5,7 @@ import re
 import time
 import traceback
 import sys
+import keyboard
 
 class CodeTranslator(object):
 
@@ -20,9 +21,11 @@ class CodeTranslator(object):
 
         self.code = ""
 
-    def convert_input(self, chunk):
-        input_code = ""
+    #split chunks into value, operand, and others
+    def split_chunk(self, chunk):
+        operand = None
         value = None
+        #extract integer(parameter) of module method
         if self.int_divider.search(chunk) != None:
             split_chunk = self.int_divider.split(chunk)
             chunk = self.tagger.pos(split_chunk[0])
@@ -30,42 +33,42 @@ class CodeTranslator(object):
             operand = self.tagger.pos(split_chunk[-1])
         else:
             chunk = self.tagger.pos(chunk)
+        return chunk, value, operand
 
+    #convert input chunks to code
+    def convert_input(self, chunk):
+        input_code = ""
+        chunk, value, operand = self.split_chunk(chunk)
+        #module name and method
+        for morph in chunk:
+                if self.input_dic.get(morph[0])!=None:
+                    input_code += self.input_dic[morph[0]]
+        #methods with return value
         if value != None:
-            for morph in chunk:
-                if self.input_dic.get(morph[0])!=None:
-                    input_code += self.input_dic[morph[0]]
+            #operand
             for morph in operand:
-                if modi_dic.operand_dic.get(morph[0])!=None:
-                    input_code += modi_dic.operand_dic[morph[0]]
-                    input_code += value
-        else:
-            for morph in chunk:
-                if self.input_dic.get(morph[0])!=None:
-                    input_code += self.input_dic[morph[0]]
+                oper_word = modi_dic.operand_dic.get(morph[0])
+                if oper_word !=None:
+                    input_code += f" {oper_word} {value}"
+            #'이':'==' case
+            if oper_word == None:
+                input_code += f" == {value}"
         return input_code
 
+    #convert output chunks to code
     def convert_output(self, chunk):
         output_code = ""
-        value = None
-        if self.int_divider.search(chunk) != None:
-            split_chunk = self.int_divider.split(chunk)
-            chunk = self.tagger.pos(split_chunk[0])
-            value = split_chunk[1]
-        else:
-            chunk = self.tagger.pos(chunk)
-
+        chunk, value, _ = self.split_chunk(chunk)
+        #module name and method
+        for morph in chunk:
+            if self.output_dic.get(morph[0])!=None:
+                output_code += self.output_dic[morph[0]]
+        #methods with parameter
         if value != None:
-            for morph in chunk:
-                if self.output_dic.get(morph[0])!=None:
-                    output_code += self.output_dic[morph[0]]
-            output_code += value + ")"
-        else:
-            for morph in chunk:
-                if self.output_dic.get(morph[0])!=None:
-                    output_code += self.output_dic[morph[0]]
+            output_code += f"{value})"
         return output_code
                 
+    #set input and output modules and their dictionaries
     def set_modules(self, chunks):
         if len(chunks) != 1:
             self.input_module = modi_dic.input_module_dic[self.tagger.nouns(chunks[0])[0]]
@@ -76,31 +79,50 @@ class CodeTranslator(object):
         self.output_dic = getattr(modi_dic, f"{self.output_module}_dic")
         self.code += f"{self.output_module} = bundle.{self.output_module}s[0]\nwhile True:\n"
 
-    def run(self):
-        # bundle = modi.MODI()
-        sentence = input("Enter: ")
-        print(self.tagger.pos(sentence))
-        try:
-            chunks = self.cond_divider.split(sentence)
-            self.set_modules(chunks)
-            #basic
-            if len(chunks)==1:
-                self.code += f"\t{self.convert_output(chunks[0])}\n\ttime.sleep(1)"
-            #advanced
-            else:
-                if modi_dic.cond_dic[chunks[1]] == "if":
-                    self.code += f"\tif {self.convert_input(chunks[0])}:\n\t\t{self.convert_output(chunks[-1])}"
-                elif modi_dic.cond_dic[chunks[1]] == "if else":
-                    self.code += f"\tif {self.convert_input(chunks[0])}:\n\t\t{self.convert_output(chunks[-1])}\n\t\ttime.sleep(1)\n"
-                    self.code += f"\telse:\n\t\t{modi_dic.else_dic[self.output_module]}\n\t\ttime.sleep(1)"
-                else:
-                    self.code += f"\twhile {self.convert_input(chunks[0])}:\n\t\t{self.convert_output(chunks[-1])}\n\t\ttime.sleep(1)\n"
-                    self.code += f"\telse:\n\t\t{modi_dic.else_dic[self.output_module]}\n\t\ttime.sleep(1)"
-            print(self.code)
-            # exec(self.code)
+    #create entire code
+    def create_code(self, sentence):
+        #split chunks into value, operand, and others
+        chunks = self.cond_divider.split(sentence)
+        #set input and output modules and their dictionaries
+        self.set_modules(chunks)
+        #basic: with no condition
+        if len(chunks)==1:
+            self.code += f"\t{self.convert_output(chunks[0])}\n\ttime.sleep(0.1)"
 
-        except:
-            traceback.print_exc()
-            print("Try again")
-        finally:
-            sys.exit()
+        #advanced: with condition
+        else:
+            #if clause
+            if modi_dic.cond_dic[chunks[1]] == "if":
+                self.code += f"\tif {self.convert_input(chunks[0])}:\n\t\t{self.convert_output(chunks[-1])}"
+            #if-else clause
+            elif modi_dic.cond_dic[chunks[1]] == "if else":
+                self.code += f"\tif {self.convert_input(chunks[0])}:\n\t\t{self.convert_output(chunks[-1])}\n\t\ttime.sleep(0.1)\n"
+                self.code += f"\telse:\n\t\t{modi_dic.else_dic[self.output_module]}\n\t\ttime.sleep(0.1)"
+            #while-else clause
+            else:
+                self.code += f"\twhile {self.convert_input(chunks[0])}:\n\t\t{self.convert_output(chunks[-1])}\n\t\ttime.sleep(0.1)\n"
+                self.code += f"\telse:\n\t\t{modi_dic.else_dic[self.output_module]}\n\t\ttime.sleep(0.1)"
+
+    def run(self):
+        #initial
+        bundle = modi.MODI()
+        sentence = ""
+    
+        while True:
+            #input sentence
+            sentence = input("Enter: ")
+            #terminate
+            if sentence == 'q':
+                break
+            self.code = ""
+            print(self.tagger.pos(sentence))
+            try:
+                self.create_code(sentence)
+                print(self.code)
+                #break to get new sentence
+                self.code += "\n\tif keyboard.is_pressed(' '):\n\t\tbreak"
+                exec(self.code)
+            except:
+                print("Try again")
+        bundle.exit()
+        sys.exit()
