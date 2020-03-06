d2c7eb53be07fc89ea46a46b98a55dc1e337c2ed
박다정 <dajungpark@bagdajeong-ui-MacBookPro.local>
2020-03-06 15:08:47 +0900

    Init bundle once

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from code_translator import CodeTranslator
 import sys
+import modi
 
 def main():
-
+    bundle = modi.MODI()
     translator = CodeTranslator()
     #translate new sentences until exit
     while True:
-        translator.run()
+        translator.run(bundle)
 
 if __name__ == '__main__':
     main()
