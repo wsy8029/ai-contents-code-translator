facfc71cc28bc346f9b4427d9af67c51a926e7e8
박다정 <dajungpark@bagdajeong-ui-MacBookPro.local>
2020-03-10 17:16:13 +0900

    Initialize speech recognition

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from code_translator import CodeTranslator
 import sys
+import speech_recognition as sr
 import modi
 
 def main():
     bundle = modi.MODI()
+    r = sr.Recognizer()
+    mic = sr.Microphone()
     translator = CodeTranslator()
     #translate new sentences until exit
     while True:
-        translator.run(bundle)
+        translator.run(bundle, r, mic)
 
 if __name__ == '__main__':
     main()
