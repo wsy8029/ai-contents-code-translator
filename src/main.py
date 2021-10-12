5b8d7d70ac34936320205c2287e7cf28d1ab272e
wsy8029 <wsy8029@gmail.com>
2021-10-12 10:53:45 +0900

    Add comments

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ import speech_recognition as sr
 import modi
 
 def main():
-    bundle = modi.MODI()
-    r = sr.Recognizer()
-    mic = sr.Microphone()
+    bundle = modi.MODI() # PyMODI 번들 선언
+    r = sr.Recognizer() # 음성 인식을 위해 SR 패키지의 recognizer 선언
+    mic = sr.Microphone() # 음성 인식을 위해 SR 패키지의 mic 선언
     translator = CodeTranslator()
     #translate new sentences until exit
     while 1:
